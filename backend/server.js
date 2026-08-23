e053ea6ec8749998d42b5376759af956f34bc514
Luan Estrela <luanestrela.oficial@gmail.com>
2026-08-23 17:11:05 -0300

    feat: Melhoria da lógica backend. Adiciona Sequelize e Docker

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 require("dotenv").config()
 
 const app = require("./app")
-
 const sequelize = require("./config/database")
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
-sequelize.sync()
+sequelize.authenticate()
     .then(() => {
+        console.log("Banco conectado com sucesso.")
         app.listen(PORT, () => {
-            console.log("Rodando na porta 3000")
-        })
+            console.log(`Rodando na porta ${PORT}`)
+        });
     })
+    .catch((error) => console.log("Erro ao conectar ao banco:", error));
