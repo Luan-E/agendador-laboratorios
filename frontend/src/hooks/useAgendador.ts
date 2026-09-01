3e6d47cbd09588d0fa5866072349260e0567b7b4
Luan Estrela <luanestrela.oficial@gmail.com>
2026-09-01 09:37:05 -0300

    fix: correção no Dockerfile

--- frontend/src/hooks/useAgendador.ts
+++ frontend/src/hooks/useAgendador.ts
@@ -31,7 +31,6 @@ export function useAgendador() {
   }, [laboratorio, data]);
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
     carregarGrade();
   }, [carregarGrade]);
 
